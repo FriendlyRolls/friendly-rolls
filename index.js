c2fa652e98fb5d3ddcdbc9d3c6346f4a78d8562a
Allan Gopez <areygopez@gmail.com>
2017-07-15 17:04:26 -0700

    added index.js entry point to server

---
+++ index.js
@@ -0,0 +1,12 @@
+/* Entry point to friendly-rolls server
+   => utilizes service-oriented architecture
+*/
+
+// setup config
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = 3000;
+app.listen(port);
+console.log('listening on http://localhost:' + port);
